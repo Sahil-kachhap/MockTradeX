a3dc89526292fa86372c18210b10bda97e804124
Sahil <sahil.kachhap111989@gmail.com>
2022-01-19 02:21:32 +0530

    Added a crypto model

---
+++ lib/Data/Models/crypto.dart
@@ -0,0 +1,17 @@
+class Crypto {
+  final String? cryptoMarket;
+  final String? cryptoSymbol;
+  final String? cryptoName;
+  final double? cryptoPrice;
+  final double? priceChange;
+  final double? currentVolume;
+
+  Crypto({
+    this.cryptoMarket,
+    this.cryptoSymbol, 
+    this.cryptoName, 
+    this.cryptoPrice, 
+    this.priceChange, 
+    this.currentVolume
+  });
+}
