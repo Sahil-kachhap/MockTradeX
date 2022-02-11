38846d87d1dd32b0c2b56975723b792f46f7887b
amperev <skt644@gmail.com>
2022-02-11 19:16:21 +0530

    added trade_screen.dart

--- lib/Presentation/Screens/front_page.dart
+++ lib/Presentation/Screens/front_page.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mock_tradex/Presentation/Screens/Funds.dart';
-import 'package:mock_tradex/Presentation/Screens/exchange_screen.dart';
-import 'package:mock_tradex/Presentation/Screens/quick_buy.dart';
+
 import 'package:mock_tradex/constants.dart';
 
 class Frontpage extends StatefulWidget {
@@ -11,30 +9,10 @@ class Frontpage extends StatefulWidget {
 }
 
 class _FrontpageState extends State<Frontpage> {
- 
-  int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Quickbuy(),
-    ExchangeScreen(),
-    Text(
-      'Code for Orders',
-      style: optionStyle,
-    ),
-   Funds()
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.indigoAccent,
+      backgroundColor: kAppBackgroundColour,
       appBar: AppBar(
         centerTitle: true,
         leading: IconButton(
@@ -54,38 +32,10 @@ class _FrontpageState extends State<Frontpage> {
         title: const Text(
           'MockTradeX',
         ),
-        backgroundColor: Color(0xff1a202c),
+        backgroundColor: kAppBarColour,
       ),
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.bubble_chart),
-            label: 'Quick Buy',
-            backgroundColor: Color(0xff1a202c),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            label: 'Exchange',
-            backgroundColor: Color(0xff1a202c),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            label: 'Orders',
-            backgroundColor: Color(0xff1a202c),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            label: 'Funds',
-            backgroundColor: Color(0xff1a202c),
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: kBottomBarTextActive,
-        onTap: _onItemTapped,
-      ),
+      body:Container(),
+
     );
   }
 }
